c0f0d64a9da300bf3ca238570e0e2f9e48f29e44
Christian-fx <akabuezechris432@gmail.com>
2026-03-31 21:26:50 +0100

    update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -23,10 +23,9 @@ export default function ProjectCard({ id, title, description, tags, image, githu
       <MotionArticle
         className="work-card"
         whileHover={{
-          y: -6, // Increased distance slightly for impact
-          scale: 1.015,
-          rotateX: -1,
-          rotateY: 1,
+          y: -10, // Increased distance slightly for impact
+          rotateX: -1.2,
+          rotateY: 1.5,
           zIndex: 10
         }}
         transition={{
